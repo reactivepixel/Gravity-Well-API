5b9712dffe5662f96b9d42ba8603a2bea877915b
Chapman <Chapman@Apextion.com>
2025-07-02 02:42:19 -0400

    refactor handler

--- handler.js
+++ handler.js
@@ -3,9 +3,6 @@ import { sequelize } from "./db/models/index.js";
 // const { sequelize } = require("db/models");
 
 export const ping = async (event) => {
-  const nodes = await sequelize.models.node.findAll();
-  console.log("All Nodes:", nodes);
-
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -19,6 +16,19 @@ export const pong = async (event) => {
     statusCode: 200,
     body: JSON.stringify({
       message: `Pong! ${process.env.FOO}`,
+    }),
+  };
+};
+
+export const db_test = async (event) => {
+  const nodes = await sequelize.models.node.findAll();
+  console.log("All Nodes:", nodes);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "All Nodes",
+      nodes: nodes,
     }),
   };
 };
